21dba9fec32b80365672735389e7e12263130f7d
Gilza Ko <gilzako@Gilzas-Mac-mini.local>
2021-05-27 17:59:52 +0900

    sdfkll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/App.css';
+import './assets/styles/App.css';
 import {IonApp, IonRouterOutlet} from "@ionic/react";
 import {IonReactRouter} from "@ionic/react-router";
 import {Route,} from "react-router-dom";
